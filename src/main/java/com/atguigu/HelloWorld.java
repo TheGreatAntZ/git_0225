742ad2be1b2b4bd2ea63afba2a5c3f6269b38b2e
zdz <thegreatant@126.com>
2021-06-27 20:56:29 +0800

    first_commit

---
+++ src/main/java/com/atguigu/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+import javax.sound.midi.Soundbank;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
